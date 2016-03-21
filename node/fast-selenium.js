80422b02fd35eb9e6c7487ca1f162872b593f50b
Vibhaj Rajan <vibhaj8@gmail.com>
2016-03-21 21:19:28 +0530

    fixes fast selenium for older version of node

--- node/fast-selenium.js
+++ node/fast-selenium.js
@@ -1,10 +1,22 @@
 var http = require('http');
-var agent = new http.Agent({
-  keepAlive: true,
-  keepAliveMsecs: 30*1000
-});
+
+var agent;
+if(process.version.substring(0,5) == 'v0.10'){
+  var AgentKeepAlive = require('agentkeepalive');
+  agent = new AgentKeepAlive({
+    keepAliveTimeout: 30*1000
+  });
+}
+else {
+  var agent = new http.Agent({
+    keepAlive: true,
+    keepAliveMsecs: 30*1000
+  });
+}
+
 var httpRequest = http.request;
 http.request = function(options, callback){
   options["agent"] = agent;
   return httpRequest(options, callback);
 }
+
